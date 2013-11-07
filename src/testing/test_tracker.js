fc59cecbaefc598bfd969d689c0e72f776a75e35
Tyler Breisacher <tbreisacher@google.com>
2013-11-07 09:53:56 -0800

    Expose a TestTracker for use in testing. ------------- Created by MOE: http://code.google.com/p/moe-java MOE_MIGRATED_REVID=55721430

---
+++ src/testing/test_tracker.js
@@ -0,0 +1,52 @@
+// Copyright 2013 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/**
+ * @fileoverview Test tracker implementation. Use this to test your
+ * code that interacts w/ a tracker (which should be most of your GA aware
+ * code).
+ *
+ * @author smckay@google.com (Steve McKay)
+ * @author tbreisacher@google.com (Tyler Breisacher)
+ */
+
+goog.provide('analytics.testing.TestTracker');
+
+goog.require('analytics.internal.ServiceTracker');
+goog.require('analytics.testing.TestChannel');
+
+
+
+/**
+ * @constructor
+ * @extends {analytics.internal.ServiceTracker}
+ */
+analytics.testing.TestTracker = function() {
+  /** @private {!analytics.testing.TestChannel} */
+  this.testChannel_ = new analytics.testing.TestChannel();
+
+  goog.base(this, this.testChannel_);
+};
+goog.inherits(
+    analytics.testing.TestTracker,
+    analytics.internal.ServiceTracker);
+
+
+/**
+ * @return {!analytics.testing.TestChannel}
+ *     the test channel owned by this TestTracker.
+ */
+analytics.testing.TestTracker.prototype.getTestChannel = function() {
+  return this.testChannel_;
+};
